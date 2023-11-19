73e27b4f87247343e853eef998b55cd745629fe7
VasiAlexandru <tk60726@gmail.com>
2023-11-19 05:06:37 +0200

    OCR added

--- src/main/java/org/scanease/scanease/api/MobileApi.java
+++ src/main/java/org/scanease/scanease/api/MobileApi.java
@@ -2,6 +2,9 @@ package org.scanease.scanease.api;
 
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
 import org.scanease.scanease.model.AllergyRequest;
 import org.scanease.scanease.model.AllergyResponse;
 import org.scanease.scanease.model.LogInRequest;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.Locale;
 import java.util.stream.Collectors;
@@ -32,11 +36,12 @@ public class MobileApi {
 
        var user =  userRepo.findById(request.getUserId());
 
-       var wordsInLabel = Arrays.stream(request.getLabel().toLowerCase(Locale.ROOT).split(",")).collect(Collectors.toSet());
 
         var response = new AllergyResponse();
 
        if(user.isPresent()){
+           String ocrText = performOCR(request.getLabel());
+           var wordsInLabel = Arrays.stream(ocrText.toLowerCase(Locale.ROOT).split(",")).collect(Collectors.toList());
 
            response.setConditions(user.get().getConditions().stream().filter(c ->{
                var ingredientWords = Arrays.stream(c.getHarmfulIngredients().toLowerCase(Locale.ROOT).split(",")).collect(Collectors.toSet());
@@ -60,6 +65,21 @@ public class MobileApi {
 
        return response;
     }
+
+    private String performOCR(String imagePath) {
+        File imageFile = new File(imagePath);
+        ITesseract instance = new Tesseract();
+        instance.setDatapath("C:\\Program Files\\Tesseract-OCR");
+
+        try{
+            return instance.doOCR(imageFile);
+        }catch (TesseractException e)
+        {
+            e.printStackTrace();
+            return"";
+        }
+    }
+
     @PostMapping("check-login")
     public LogInResponse checkLogIn(@RequestBody LogInRequest request)
     {
@@ -76,6 +96,8 @@ public class MobileApi {
                     log.info("Got request: {}  found user {}", request,user);
                     break;
                 }
+                else
+                    response.setId(-1);
             }
         }
 
