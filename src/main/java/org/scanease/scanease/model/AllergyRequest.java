99a5822a131c1239370497811e104e3dca905945
VasiAlexandru <tk60726@gmail.com>
2023-11-19 10:38:06 +0200

    OCR added

--- src/main/java/org/scanease/scanease/model/AllergyRequest.java
+++ src/main/java/org/scanease/scanease/model/AllergyRequest.java
@@ -6,7 +6,6 @@ import org.springframework.web.multipart.MultipartFile;
 @Data
 public class AllergyRequest {
 
-    private String label;
     //here we add the label string from mobile app
     private int userId;
     private MultipartFile image;
